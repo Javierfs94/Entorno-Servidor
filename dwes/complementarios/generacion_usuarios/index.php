0d1a467e5c031780e2422a89f29c6ca9a00886e2
Javierfs <javifs94@gmail.com>
2020-04-25 18:30:02 +0200

    Subida ejercicio generar usuarios

--- dwes/complementarios/generacion_usuarios/index.php
+++ dwes/complementarios/generacion_usuarios/index.php
@@ -14,7 +14,7 @@
 
 <?php
   // Includes
-  // include "";
+  include "funciones.php";
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +31,7 @@
 <body>
 
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "root";
@@ -46,24 +47,98 @@ if ($conn->connect_error) {
 // sql to create table
 $sql = "CREATE TABLE usuarios (
 id INT(3) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-nombre VARCHAR(30) NOT NULL,
-apellido1 VARCHAR(30) NOT NULL,
-apellido2 VARCHAR(30) NOT NULL,
-compuesto VARCHAR(30) NOT NULL
+usuario VARCHAR(30) NOT NULL
 )";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Table generar_usuarios created successfully";
+    echo "<p>Table generar_usuarios created successfully</p>";
 } else {
-    echo "Error creating table: " . $conn->error;
+    echo "<p>Error creating table: " . $conn->error . "</p>";
 }
 
 $conn->close();
-?> 
+
+
+if (isset($_POST["enviar"])) {
+    $file = fopen("alumnos.txt", "r");
+    $contador = 0;
+    $arrayUsers = array();
+    $contadorRepetidos=0;
+    
+    $origen = fopen($_POST["fichero"], "r");
+    
+    do {
+    
+        $cadena = normaliza(strtolower(fgets($origen)));
+    
+        if ($contador > 7) {
+            $nombreCompleto = explode(" ", $cadena);
+            $user = substr($nombreCompleto[0], 0, 2).substr($nombreCompleto[1], 0, 2).substr($nombreCompleto[2], 0, 2);
+            do {
+                if (in_array($user, $arrayUsers)) {
+                    $user = $user.$contadorRepetidos;
+                    $contadorRepetidos++;
+                }
+                array_push($arrayUsers, $user);
+            } while (!in_array($user, $arrayUsers));
+        }
+    
+        $contador++;
+        
+    } while (!feof($origen));
+    
+    fclose($origen);
+
+
+if ($_POST["tipo"] == "mysql") {
+    $salida = fopen("salida.txt","w");
+    foreach ($arrayUsers as $key => $usuario) {
+        
+        $comando1 = "CREATE USER '$usuario'@'localhost' IDENTIFIED BY '$usuario' \n";
+        $comando2 = "GRANT ALL PRIVILEGES ON * . * TO '$usuario'@'localhost' \n";
+
+        fwrite($salida, $comando1);
+        fwrite($salida, $comando2);
+    }
+    fclose($salida);
+}
+
+if ($_POST["tipo"] == "linux") {
+    $salida = fopen("salida.txt","w");
+    foreach ($arrayUsers as $key => $usuario) {
+
+        $comando1 = "sudo useradd $usuario \n";
+        $comando2 = "sudo passwd $usuario \n";
+
+        fwrite($salida, $comando1);
+        fwrite($salida, $comando2);
+    }
+    fclose($salida);
+}
+    
+}
+
+?>
+
+
+<form method="post" action="index.php" name="signin-form">
+    <div class="form-element">
+        <label>Tipo Comando</label>
+        <select name="tipo">
+        <option value="mysql">MySql</option>
+        <option value="linux">Linux</option>
+        </select>
+    </div>
+    <div class="form-element">
+        <label>Fichero</label>
+        <input type="text" name="fichero" required />
+    </div>
+    <button type="submit" name="enviar" value="enviar">Enviar</button>
+</form>
 
 <?php
   // Boton para ir al repositorio del ejercicio
-    echo "<br/><a href=''><button>Repositorio</button></a>";    
+    echo "<br/><a href='https://github.com/Javierfs94/Entorno-Servidor/tree/master/dwes/complementarios/generacion_usuarios'><button>Repositorio</button></a>";    
 ?>
 
 </body>
